36918b3075c01f09904fdbe15e70513fae788fc2
unknown <sudeendracse@gmail.com>
2018-10-03 02:30:59 +0530

    first to JamStack Hackathon

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Layout from '../components/layout'
+import { Link } from 'gatsby'
+import React from 'react'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi, Iam Sudeendra G :) </h1>
+    <ul>
+    <li> Working as Software Engineer with 2+ year experience in Digital Harbor Inc, Bangalore, India.
+      <br/> Currently Developing an application using Angular, spring boot ,docker,Sql Server ,etc... </li>
+       <li>Iam a polyglot, passionate to learn new things</li>
+   </ul>
+       ..... Let's get Connected --> <a href="https://www.linkedin.com/in/sudeendrag/" target="_blank">@Linkedin</a>  
+    <br/>
+  </Layout>
+)
+
+export default IndexPage
